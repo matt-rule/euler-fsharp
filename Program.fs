950288dfec42bff3d610939d64bed8fd97a36391
matt-rule <matt.rule.github@gmail.com>
2020-01-18 23:18:43 +0000

    First Euler problem

--- Program.fs
+++ Program.fs
@@ -1,6 +1,22 @@
 ﻿open System
 
+let problem1 =
+    seq { 1..999 }
+    |> Seq.where(fun x -> x % 3 = 0 || x % 5 = 0)
+    |> Seq.sum
+
+let problem2 : int =
+    let fibonacci = Seq.unfold(fun x -> if x > 4 then Some(3) else None)
+    fibonacci 10
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let problem : int = Int32.Parse(argv.[0])
+    let result : string =
+        match problem with
+        | 1 -> problem1.ToString()
+        | 2 -> "2"
+        | 3 -> "3"
+        | _ -> "Hello World from F#!"
+    Console.WriteLine(result)
+    0
