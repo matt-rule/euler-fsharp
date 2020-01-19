444fd5534479a6f22d7fbcf0c9f8627659da4579
matt-rule <matt.rule.github@gmail.com>
2020-01-19 22:30:13 +0000

    Answers to first three problems

--- Program.fs
+++ Program.fs
@@ -5,9 +5,29 @@ let problem1 =
     |> Seq.where(fun x -> x % 3 = 0 || x % 5 = 0)
     |> Seq.sum
 
-let problem2 : int =
-    let fibonacci = Seq.unfold(fun x -> if x > 4 then Some(3) else None)
-    fibonacci 10
+let problem2 =
+    let fibonacci = Seq.unfold(fun (x, y) -> Some(x + y, (y, x + y)))
+
+    Seq.concat [
+        seq {1; 2};
+        fibonacci (1, 2)
+    ]
+    |> Seq.takeWhile(fun x -> x <= 4000000)
+    |> Seq.where(fun x -> x % 2 = 0)
+    |> Seq.sum
+
+let problem3 =
+    let primeFactors n =
+        Seq.unfold(fun x ->
+            if x = 1L then None else
+                let divisor = seq { 2L..x } |> Seq.find(fun y -> x % y = 0L)
+                Some(divisor, x / divisor)
+        ) n
+
+    let l = primeFactors 600851475143L |> Seq.toList
+
+    primeFactors 600851475143L
+    |> Seq.max
 
 [<EntryPoint>]
 let main argv =
@@ -15,8 +35,8 @@ let main argv =
     let result : string =
         match problem with
         | 1 -> problem1.ToString()
-        | 2 -> "2"
-        | 3 -> "3"
+        | 2 -> problem2.ToString()
+        | 3 -> problem3.ToString()
         | _ -> "Hello World from F#!"
     Console.WriteLine(result)
     0
