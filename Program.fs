667bc3f63b980dc3b2a1d783cce687faa29414b0
matt-rule <matt.rule.github@gmail.com>
2020-01-20 23:28:28 +0000

    Problem 4

--- Program.fs
+++ Program.fs
@@ -4,39 +4,43 @@ let problem1 =
     seq { 1..999 }
     |> Seq.where(fun x -> x % 3 = 0 || x % 5 = 0)
     |> Seq.sum
+    |> string
 
 let problem2 =
-    let fibonacci = Seq.unfold(fun (x, y) -> Some(x + y, (y, x + y)))
-
-    Seq.concat [
-        seq {1; 2};
-        fibonacci (1, 2)
-    ]
+    Seq.unfold(fun (x, y) -> Some(x + y, (y, x + y))) (0, 1)
     |> Seq.takeWhile(fun x -> x <= 4000000)
     |> Seq.where(fun x -> x % 2 = 0)
     |> Seq.sum
+    |> string
 
 let problem3 =
-    let primeFactors n =
-        Seq.unfold(fun x ->
-            if x = 1L then None else
-                let divisor = seq { 2L..x } |> Seq.find(fun y -> x % y = 0L)
-                Some(divisor, x / divisor)
-        ) n
-
-    let l = primeFactors 600851475143L |> Seq.toList
-
-    primeFactors 600851475143L
+    600851475143L
+    |> Seq.unfold(fun x ->
+        if x = 1L then None else
+            let y = seq { 2L..x } |> Seq.find(fun y -> x % y = 0L)
+            Some(y, x / y)
+    )
     |> Seq.max
+    |> string
 
-[<EntryPoint>]
-let main argv =
-    let problem : int = Int32.Parse(argv.[0])
-    let result : string =
-        match problem with
-        | 1 -> problem1.ToString()
-        | 2 -> problem2.ToString()
-        | 3 -> problem3.ToString()
-        | _ -> "Hello World from F#!"
-    Console.WriteLine(result)
-    0
+let problem4 =
+    seq { 1..999 }
+    |> Seq.collect(fun x ->
+        seq { x+1..999 }
+        |> Seq.map (fun y -> x*y |> string)
+        |> Seq.where (fun y -> y = (y |> seq |> Seq.rev |> String.Concat))
+        |> Seq.map(Int32.Parse)
+    )
+    |> Seq.max
+    |> string
+
+let answers = [|
+    (problem1, "233168")
+    (problem2, "4613732")
+    (problem3, "6857")
+    (problem4, "906609")
+|]
+
+answers
+|> Array.forall(fun (x, y) -> x = y)
+|> Console.WriteLine
