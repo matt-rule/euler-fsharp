655aace26ded7145ffc131dcd7527e5e0791c9d2
matt-rule <matt.rule.github@gmail.com>
2020-03-07 23:57:20 +0000

    Add some benchmarks; begin to optimise problem 23

--- EulerSolutionBenchmarks.fs
+++ EulerSolutionBenchmarks.fs
@@ -56,18 +56,42 @@ type EulerSolutionBenchmarks() =
     // member this.Problem13() =
     //     EulerSolving.problem13 (File.ReadAllLines "data/problem13.txt")
 
-    [<Benchmark>]
-    member this.Problem14() =
-        EulerSolving.problem14 1000000L
+    // [<Benchmark>]
+    // member this.Problem14() =
+    //     EulerSolving.problem14 1000000L
 
     // [<Benchmark>]
     // member this.Problem15() =
-    //     EulerSolving.problem15
+    //     EulerSolving.problem15 20
 
     // [<Benchmark>]
     // member this.Problem16() =
-    //     EulerSolving.problem16
+    //     EulerSolving.problem16 1000
 
     // [<Benchmark>]
     // member this.Problem17() =
-    //     EulerSolving.problem17
+    //     EulerSolving.problem17 1000
+
+    // [<Benchmark>]
+    // member this.problem18() =
+    //     EulerSolving.problem18 (File.ReadAllLines "data/problem18.txt")
+
+    // [<Benchmark>]
+    // member this.Problem19() =
+    //     EulerSolving.problem19
+
+    // [<Benchmark>]
+    // member this.Problem20() =
+    //     EulerSolving.problem20 100
+
+    // [<Benchmark>]
+    // member this.Problem21() =
+    //     EulerSolving.problem21 10000
+
+    // [<Benchmark>]
+    // member this.Problem22() =
+    //     EulerSolving.problem22 (File.ReadAllText "data/problem22.txt")
+
+    [<Benchmark>]
+    member this.Problem23() =
+        EulerSolving.problem23 28123
